c6f12386453f33d996215e41cb22900b172067b4
Abhishek Yadav <nhykhlb@gmail.com>
2020-12-10 16:55:06 +0530

    project build successfull

--- app/src/main/java/com/deeplearning/plantclassification/customview/ResultView.java
+++ app/src/main/java/com/deeplearning/plantclassification/customview/ResultView.java
@@ -1,4 +1,9 @@
 package com.deeplearning.plantclassification.customview;
 
+import com.deeplearning.plantclassification.tflite.Classifier;
+
+import java.util.List;
+
 public interface ResultView {
+    public void setResults(final List<Classifier.Recognition> results);
 }
